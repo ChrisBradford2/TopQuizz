3ac25e78af36180ff09281fc3c8d435d9664f62c
Nicolas Barbarisi <nico.bar2012@gmail.com>
2021-12-13 15:45:36 +0100

    Manage user actions

--- app/src/main/java/com/koholnt/topquiz/MainActivity.java
+++ app/src/main/java/com/koholnt/topquiz/MainActivity.java
@@ -1,6 +1,10 @@
 package com.koholnt.topquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
@@ -16,5 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayButton.setEnabled(false);
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mPlayButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user just clicked
+            }
+        });
     }
 }
